6a01e44eec0d2b36292ba6a57425cd27da9fa3f3
peter.m <peter.m@kakaocorp.com>
2020-12-01 22:47:32 +0900

    Initial Commit

---
+++ referFolderTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  referFolderTest
+//
+//  Created by Peter.M on 2020/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ referFolderTest/referFolderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  referFolderTestApp.swift
+//  referFolderTest
+//
+//  Created by Peter.M on 2020/12/01.
+//
+
+import SwiftUI
+
+@main
+struct referFolderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
